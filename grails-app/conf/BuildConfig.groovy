fdde512c3cc63447eb1efc884fc7eb0efa3efc26
Paul Osborne <hello@paulosborne.me.uk>
2013-07-04 22:42:23 +0100

    Added Spock and Mongo.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -3,35 +3,30 @@ grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
+
+	inherits("global")
+    log "warn"
+	
     repositories {
         grailsCentral()
         mavenCentral()
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenLocal()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
+        mavenLocal()
     }
+	
     dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-
-        // runtime 'mysql:mysql-connector-java:5.1.21'
-    }
+		test('org.spockframework:spock-grails-support:0.7-groovy-2.0') {
+			export = false
+		}
+	}
 
     plugins {
-        build(":tomcat:$grailsVersion",
-              ":release:2.2.1",
-              ":rest-client-builder:1.0.3") {
-            export = false
-        }
+		build(':release:2.2.0', ':rest-client-builder:1.0.3') {
+		    export = false
+		}
+		compile ":mongodb:1.2.0"
+		test(':spock:0.7') {
+		    export = false
+			exclude 'spock-grails-support'
+		}
     }
 }

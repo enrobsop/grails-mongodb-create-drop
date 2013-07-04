a04fa8c12613474fa9a3d55a48ab83d2a0968eeb
Paul Osborne <hello@paulosborne.me.uk>
2013-07-04 16:59:23 +0100

    Initialised plugin project.

---
+++ MongoBootstrapUtilsGrailsPlugin.groovy
@@ -0,0 +1,29 @@
+class MongoBootstrapUtilsGrailsPlugin {
+    def version = "0.1.SNAPSHOT"
+    def grailsVersion = "2.0 > *"
+	def dependsOn = [
+		'mongodb': "* > 1.0.0.RC3",
+		'burning-image': "* > 0.5.0"
+	]
+    def pluginExcludes = [
+		"grails-app/controllers/**',
+		"grails-app/domain/**',
+		"grails-app/i18n/**',
+		"grails-app/views/**/*"
+		"web-app/**"    
+	]
+
+    def title = "Mongo BootStrap Utils" // Headline display name of the plugin
+    def author = "Paul Osborne"
+    def authorEmail = "hello@paulosborne.me.uk"
+    def description = '''\
+Provides BootStrap utilities to simulate database dropCreate behaviour when working with MongoDB. 
+'''
+
+    def documentation = "https://github.com/enrobsop/grails-mongo-bootstrap-utils/wiki"
+	def license = "APACHE"
+	def organization = [ name: "Morley Computing", url: "http://www.morley-computing.co.uk/" ]
+	def issueManagement = [ system: "GitHub", url: "https://github.com/enrobsop/grails-mongo-bootstrap-utils/issues" ]
+	def scm = [ url: "https://github.com/enrobsop/grails-mongo-bootstrap-utils/" ]
+
+}
